df393c470f65d72046418d85779f5169b65891e4
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-02-05 00:47:11 +0200

    intial menu select

---
+++ MainMenu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+#description file : menu to create DB or list DB or Drop DB
+
+
+###### Create DB
+#Menu-Main select 
+#create DB ,list DB,Connect BD ,Drop DB
+echo "Welcome,Please choose from the Menu select "
+select element in CreateDB ListDB ConnectDB DropDB Exist
+do 
+
+    case $element in
+     "CreateDB") echo $element
+       ;;
+       "ListDB") echo $element
+       ;;
+       "ConnectDB") echo $element
+       ;;
+       "DropDB") echo $element
+       ;;
+       "Exist") break
+       ;;
+       *)
+         echo "Please,use vaild select"
+       ;;      
+    esac
+done
