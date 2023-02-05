b30a07f24991fac7af7f0dcb395318eedd9cb66a
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-02-05 22:54:46 +0200

    inital ListDB.sh

---
+++ ListDB.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+#description for this script: List DB
+
+#validation list Directory file only---> okay
+#validation directory database is exist or no --->okay
+#validation DB is exist or no ---> okay
+
+#enhancement to remove slash "/" in the end of character names DB
+
+if [ -d ./DB ]
+then
+   ls -F ./DB |  grep / #list all directories in DB only
+   if [ $? -ne 0 ]
+   then
+    echo "No exist Any Databases in DB Storage or Directory"
+   fi
+else
+   echo "No exist Any Databases"
+fi
