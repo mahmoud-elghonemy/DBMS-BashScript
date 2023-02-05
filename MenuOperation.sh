b6d4fc9e880cadc6acd64d2d3d244dafc18c75d2
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-02-06 01:43:54 +0200

    update MenuOperation

--- MenuOperation.sh
+++ MenuOperation.sh
@@ -18,6 +18,31 @@
 echo "Select what do you do on Database "
 select operation in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select From Table" "Delete From Table" "Update Table" "Exit"
 do
-echo $operation
+
+        case $operation in 
+         "Create Table")
+          echo $operation
+         ;;
+         "List Tables")
+         echo $operation
+         ;;
+         "Drop Table")
+         echo $operation
+        ;;
+         "Insert into Table")
+         echo $operation
+         ;;
+         "Select From Table")
+         ;;
+         "Delete From Table")
+         ;;
+         "Update Table")
+         ;;
+         "Exit")
+         ;;
+         *)
+         echo "Please , choose correct operation"
+         ;;
+        esac
 done
 
