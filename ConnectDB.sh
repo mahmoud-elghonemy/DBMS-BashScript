ee434f83251dd7bbc8c53de5edea89545357299d
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-02-14 00:21:35 +0200

    delete unseful comments

--- ConnectDB.sh
+++ ConnectDB.sh
@@ -4,27 +4,6 @@
 #Connect DB 
 #two approach 1)select DB 2)input user
 
-#create array name DB to connect 
-# arrDB = `((ls -F ./DB |  grep / | cut -f1 /))`
-# echo $arrDB
-# echo "Select Which Database need to connect"
-# if [ -d ./DB ] 
-# then 
-#  arrDB="$(ls -F ./DB |  grep / | cut -d '/' -f1)"
-#      if [ ${#arrDB} -ne 0 ] 
-#      then   
-#         select DB in $arrDB Exit
-#             do
-#              case $DB in 
-
-#             done
-#      else 
-#       echo "Not Exist any DataBases"
-#      fi
-# else 
-#  echo "Not Exist any DataBases"
-# fi 
-
 read -p "Please,Enter DataBase Name To Connect: " ConDBName
 #check $ConDBName and check DB is exist or no 
 #valid nanme 
