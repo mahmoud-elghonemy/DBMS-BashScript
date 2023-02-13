83f80dfb76d209c2ae7f56713fee8a6eab899126
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-02-14 00:20:37 +0200

    update select sh

--- Select.sh
+++ Select.sh
@@ -4,8 +4,24 @@
 #select all table -->okay
 #check table is exist or no --->okay
 
-#select column check is exist in table or no
-#select this column if it is exist 
+#select column check is exist in table or no --->okay
+#select this column if it is exist --->okay
+function checkInt()
+{
+    local re='^([0-9]+)*$'
+    (( ${#1} > 16 )) && return 1
+    [[ "$1" =~ $re ]]
+    
+}
+
+
+testValidInt() {
+  if checkInt "$1"; then 
+    return 0
+  else
+     return 1 
+  fi
+}
 
 
 
@@ -17,30 +33,51 @@ then
 echo "Please,choose what do you want to select from menu? "
 select sel in "Select all data From Table" "Select Column" "Select Row" "Exit"
 do 
-
+     Numcol=($(awk 'BEGIN{FS=":";} END{print NR}' ./$selTable.Mdata)) 
+     Numrows=($(awk 'BEGIN{FS=":";} END{print NR}' ./$selTable)) 
      case $sel in 
        "Select all data From Table") 
            echo "Select this Table"
+           echo "-------------------------------------"
            cat $selTable
-      #      awk 'BEGIN{FS=":";ORS=" "; print `awk -F: '{print $1}' test.Mdata`;} {for(i=1; i<=NF; i++) {print $i,"\t\t";} print "\n" } END { print "---------\nTable Complete\n" }' ./$selTable
-   
-      # awk 'BEGIN{FS=":";ORS=" ";OFS="\t\t" ; print "id\t\tfname\t\tlname\t\t\tage\t\t\n---------------------------------------\n";} {for(i=1; i<=NF; i++) {print $i,"\t";} print "\n" } END { print "---------\nTable Complete\n" }' ./$selTable
-   
-    #    awk 'BEGIN { FS=":"; print "User\t\tUID\t\tGID\t\tHome\t\tShell\n---------------------------------------"; }
-    #        {print $1,"\t\t",$3,"\t\t",$4,"\t\t",$6,"\t\t",$7;}
-    #         END { print "---------\nFile Complete" }' /etc/passwd
+           echo "-------------------------------------"
 
-# awk '{printf "%s ",$2}' file
         ;;
        
        "Select Column")
-
+    
        read -p "Enter Column Number: " cnum
+       testValidInt "$cnum"
+       if [ $? -eq 0  ]
+       then
+       if [ $cnum -le $Numcol ]
+       then
+       echo "-------------------------------------"
        cat $selTable | cut -d: -f$cnum # -f field 
+       echo "-------------------------------------"
+       else 
+       echo "Doesn't Exist $cnum Numbers Columns"
+       fi
+       else 
+       echo "Sorry,You Must enter number column only"
+       fi
        ;;
        "Select Row")
        read -p "Enter Row Number: " nrow
-       sed -n "$nrow p" $selTable  # -n  for number of row
+      testValidInt "$nrow"
+       if [ $? -eq 0 ]
+       then
+       if [ $nrow -le $Numrows ]
+       then
+       echo "-------------------------------------"
+       sed -n "$nrow p" ./$selTable  # -n  for number of row
+       echo "-------------------------------------"
+       else 
+       echo "Doesn't Exist $nrow Numbers Rows"
+       fi
+       else 
+       echo "echo "Sorry,You Must enter number row only""
+       fi
        ;;
        "Exit")
          break
